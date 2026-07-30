b010d6fb732bfc132b4c34598953f62cb9cbda2b
Ansh Pandey <oneanshpandey@gmail.com>
2026-07-30 19:41:44 +0530

    Time: 799 ms (8.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 262-trips-and-users/trips-and-users.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT 
+    request_at AS Day, 
+    ROUND(SUM(status != 'completed') / COUNT(*), 2) AS `Cancellation Rate`
+FROM Trips
+WHERE 
+    client_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
+    AND driver_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
+    AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY request_at;
