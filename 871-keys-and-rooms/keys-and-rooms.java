1a9e457251140087d5864004c471d9b0e1dbb97a
Ansh Pandey <oneanshpandey@gmail.com>
2026-08-02 09:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (26.38%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms.size();
+
+        // List<List<Integer>>graph = new ArrayList<>();
+        boolean[]ans = new boolean[n];
+        dfs(0 , ans , rooms);
+        for(boolean b : ans){
+            if(!b){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void dfs(int vertex , boolean[]vis , List<List<Integer>>graph){
+        vis[vertex]=true;
+        List<Integer>list = graph.get(vertex);
+        for(int i : list){
+            if(vis[i]==false){
+                dfs(i , vis , graph);
+            }
+        }
+    }
+}
